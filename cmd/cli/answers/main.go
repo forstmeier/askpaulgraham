2c88821c8b8604b92b98d853a65365b4a98ad6a2
forstmeier <john.forstmeier@gmail.com>
2021-12-17 08:49:20 -0500

    Split helper CLI into summaries/answers packages

---
+++ cmd/cli/answers/main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/forstmeier/askpaulgraham/pkg/cnt"
+)
+
+const (
+	answerFilename  = "answer.json"
+	answersFilename = "answers.jsonl"
+)
+
+const (
+	getAction  = "get"
+	setAction  = "set"
+	singleSize = "single"
+	bulkSize   = "bulk"
+)
+
+type samConfigTOML struct {
+	DataBucket   string `toml:"data_bucket"`
+	OpenAIAPIKey string `toml:"open_ai_api_key"`
+}
+
+type answerJSON struct {
+	Text     string `json:"text"`
+	Metadata string `json:"metadata"`
+}
+
+func main() {
+	ctx := context.Background()
+
+	action := flag.String("action", "get", `action to perform ("get" or "set")`)
+	size := flag.String("size", "single", `size of the action ("single" or "bulk")`)
+	postID := flag.String("id", "", "blog post id")
+
+	flag.Parse()
+
+	if *action != getAction && *action != setAction {
+		log.Fatalf("invalid action: %s", *action)
+	}
+
+	if *size != singleSize && *size != bulkSize {
+		log.Fatalf("invalid size: %s", *size)
+	}
+
+	if *size == singleSize && *postID == "" {
+		log.Fatal("error invalid arguments: argument 'id' is required for 'single' operation")
+	}
+
+	config := samConfigTOML{}
+	configContent, err := ioutil.ReadFile("samconfig.toml")
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+	}
+	if err := toml.Unmarshal(configContent, &config); err != nil {
+		log.Fatalf("error unmarshalling config file: %v", err)
+	}
+
+	newSession, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+	if err != nil {
+		log.Fatalf("error creating aws session: %v", err)
+	}
+
+	s3Client := s3.New(newSession)
+	cntClient := cnt.New()
+
+	if *action == getAction {
+		if *size == singleSize {
+			postURL := fmt.Sprintf("http://www.paulgraham.com/%s.html", *postID)
+			text, err := cntClient.GetText(ctx, postURL)
+			if err != nil {
+				log.Fatalf("error getting text: %v", err)
+			}
+
+			bodyBytes, err := json.Marshal(answerJSON{
+				Metadata: *postID,
+				Text:     *text,
+			})
+			if err != nil {
+				log.Fatalf("error marshalling answer: %v", err)
+			}
+
+			if err := os.WriteFile(answerFilename, bodyBytes, 0644); err != nil {
+				log.Fatalf("error writing answer file: %v", err)
+			}
+
+		} else if *size == bulkSize {
+			items, err := cntClient.GetItems(ctx, "http://www.aaronsw.com/2002/feeds/pgessays.rss")
+			if err != nil {
+				log.Fatalf("error getting items: %v", err)
+			}
+
+			answersBody := bytes.Buffer{}
+			encoder := json.NewEncoder(&answersBody)
+			for _, item := range items {
+				if strings.Contains(item.Link, "1638975042") {
+					continue
+				}
+
+				text, err := cntClient.GetText(ctx, item.Link)
+				if err != nil {
+					log.Fatalf("error getting text: %v", err)
+				}
+
+				_, file := path.Split(item.Link)
+				id := strings.Replace(file, ".html", "", -1)
+
+				if err := encoder.Encode(answerJSON{
+					Text:     *text,
+					Metadata: id,
+				}); err != nil {
+					log.Fatalf("error encoding answer: %v", err)
+				}
+			}
+
+			if err := os.WriteFile(answersFilename, answersBody.Bytes(), 0644); err != nil {
+				log.Fatalf("error writing answers file: %v", err)
+			}
+		}
+
+	} else if *action == setAction {
+		filename := ""
+		if *size == singleSize {
+			filename = answerFilename
+		} else if *size == bulkSize {
+			filename = answersFilename
+		}
+
+		bodyBytes, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatalf("error reading file: %v", err)
+		}
+
+		if *size == singleSize {
+			newAnswer := answerJSON{}
+			if err := json.Unmarshal(bodyBytes, &newAnswer); err != nil {
+				log.Fatalf("error unmarshalling answer: %v", err)
+			}
+
+			getAnswersResp, err := s3Client.GetObject(&s3.GetObjectInput{
+				Bucket: &config.DataBucket,
+				Key:    aws.String(answersFilename),
+			})
+			if err != nil {
+				log.Fatalf("error getting answers: %v", err)
+			}
+
+			answersBody := bytes.Buffer{}
+
+			encoder := json.NewEncoder(&answersBody)
+			if err := encoder.Encode(newAnswer); err != nil {
+				log.Fatalf("error encoding new answer: %v", err)
+			}
+			decoder := json.NewDecoder(getAnswersResp.Body)
+
+			for decoder.More() {
+				var answer answerJSON
+				if err := decoder.Decode(&answer); err == io.EOF {
+					break
+				} else if err != nil {
+					log.Fatalf("error decoding answer: %v", err)
+				}
+
+				if newAnswer.Metadata != answer.Metadata {
+					if err := encoder.Encode(answer); err != nil {
+						log.Fatalf("error encoding old answer: %v", err)
+					}
+				}
+			}
+
+			bodyBytes = answersBody.Bytes()
+		}
+
+		_, err = s3Client.PutObject(&s3.PutObjectInput{
+			Bucket: &config.DataBucket,
+			Key:    aws.String(answersFilename),
+			Body:   bytes.NewReader(bodyBytes),
+		})
+		if err != nil {
+			log.Fatalf("error putting answers: %v", err)
+		}
+	}
+}
---
+++ cmd/cli/summaries/main.go
@@ -0,0 +1,204 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/s3"
+
+	"github.com/forstmeier/askpaulgraham/pkg/cnt"
+	"github.com/forstmeier/askpaulgraham/pkg/nlp"
+)
+
+const (
+	summaryFilename   = "summary.csv"
+	summariesFilename = "summaries.csv"
+)
+
+const (
+	getAction  = "get"
+	setAction  = "set"
+	singleSize = "single"
+	bulkSize   = "bulk"
+)
+
+type samConfigTOML struct {
+	DataBucket   string `toml:"data_bucket"`
+	OpenAIAPIKey string `toml:"open_ai_api_key"`
+}
+
+type summariesJSON struct {
+	Items []summaryJSON `json:"Items"`
+}
+
+type summaryJSON struct {
+	ID      string `json:"id"`
+	URL     string `json:"url"`
+	Title   string `json:"title"`
+	Summary string `json:"summary"`
+}
+
+func main() {
+	ctx := context.Background()
+
+	action := flag.String("action", "get", `action to perform ("get" or "set")`)
+	size := flag.String("size", "single", `size of the action ("single" or "bulk")`)
+	postID := flag.String("id", "", "blog post id")
+
+	flag.Parse()
+
+	if *action != getAction && *action != setAction {
+		log.Fatalf("error invalid action: %s", *action)
+	}
+
+	if *size != singleSize && *size != bulkSize {
+		log.Fatalf("error invalid size: %s", *size)
+	}
+
+	if *size == singleSize && *postID == "" {
+		log.Fatal("error invalid arguments: argument 'id' is required for 'single' operation")
+	}
+
+	config := samConfigTOML{}
+	configContent, err := os.ReadFile("samconfig.toml")
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+	}
+	if err := toml.Unmarshal(configContent, &config); err != nil {
+		log.Fatalf("error unmarshalling config file: %v", err)
+	}
+
+	newSession, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1"),
+	})
+	if err != nil {
+		log.Fatalf("error creating aws session: %v", err)
+	}
+
+	cntClient := cnt.New()
+	nlpClient := nlp.New(newSession, config.OpenAIAPIKey, config.DataBucket)
+	s3Client := s3.New(newSession)
+
+	if *action == getAction {
+		items, err := cntClient.GetItems(ctx, "http://www.aaronsw.com/2002/feeds/pgessays.rss")
+		if err != nil {
+			log.Fatalf("error getting items: %v", err)
+		}
+
+		summaries := []summaryJSON{}
+		for _, item := range items {
+			if (*size == bulkSize) || (*size == singleSize && strings.Contains(item.Link, *postID)) {
+				text, err := cntClient.GetText(ctx, item.Link)
+				if err != nil {
+					log.Fatalf("error getting text: %v", err)
+				}
+
+				summary, err := nlpClient.GetSummary(ctx, *text)
+				if err != nil {
+					log.Fatalf("error getting summary: %v", err)
+				}
+
+				_, file := path.Split(item.Link)
+				id := strings.Replace(file, ".html", "", -1)
+
+				summaries = append(summaries, summaryJSON{
+					ID:      id,
+					URL:     item.Link,
+					Title:   item.Title,
+					Summary: *summary,
+				})
+			}
+		}
+
+		summariesBytes, err := json.Marshal(summariesJSON{
+			Items: summaries,
+		})
+
+		filename := ""
+		if *size == singleSize {
+			filename = summaryFilename
+		} else if *size == bulkSize {
+			filename = summariesFilename
+		}
+
+		if err := os.WriteFile(filename, summariesBytes, 0644); err != nil {
+			log.Fatalf("error writing summaries file: %v", err)
+		}
+
+	} else if *action == setAction {
+		filename := ""
+		if *size == singleSize {
+			filename = summaryFilename
+		} else if *size == bulkSize {
+			filename = summariesFilename
+		}
+
+		summariesBytes, err := os.ReadFile(filename)
+		if err != nil {
+			log.Fatalf("error reading summaries file: %v", err)
+		}
+
+		summaries := summariesJSON{}
+		if err := json.Unmarshal(summariesBytes, &summaries); err != nil {
+			log.Fatalf("error unmarshalling summaries file: %v", err)
+		}
+
+		putRequests := []*dynamodb.WriteRequest{}
+		for _, item := range summaries.Items {
+			putRequests = append(putRequests, &dynamodb.WriteRequest{
+				PutRequest: &dynamodb.PutRequest{
+					Item: map[string]*dynamodb.AttributeValue{
+						"id": {
+							S: aws.String(item.ID),
+						},
+						"url": {
+							S: aws.String(item.URL),
+						},
+						"title": {
+							S: aws.String(item.Title),
+						},
+						"summary": {
+							S: aws.String(item.Summary),
+						},
+					},
+				},
+			})
+
+			if _, err := s3Client.PutObject(&s3.PutObjectInput{
+				Bucket: aws.String(config.DataBucket),
+				Key:    aws.String(fmt.Sprintf("%s.md", item.ID)),
+				Body:   strings.NewReader(item.Summary),
+			}); err != nil {
+				log.Fatalf("error putting summary markdown file: %v", err)
+			}
+		}
+
+		dynamoDBClient := dynamodb.New(newSession)
+
+		summariesTableName := ""
+		tables, err := dynamoDBClient.ListTables(&dynamodb.ListTablesInput{})
+		for _, tableName := range tables.TableNames {
+			if strings.Contains(*tableName, "summariesTable") {
+				summariesTableName = *tableName
+			}
+		}
+		_, err = dynamoDBClient.BatchWriteItem(&dynamodb.BatchWriteItemInput{
+			RequestItems: map[string][]*dynamodb.WriteRequest{
+				summariesTableName: putRequests,
+			},
+		})
+		if err != nil {
+			log.Fatalf("error batch writing summaries: %v", err)
+		}
+	}
+}
